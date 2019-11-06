dc9f8e7edbb051715d70343aac1f945e3bab094d
Xu Shane <xusheng0711@gmail.com>
2019-11-06 15:36:58 +0800

    add hie

--- shell.nix
+++ shell.nix
@@ -1,6 +1,7 @@
 with import <nixpkgs> {};
 let
   lrwh = (import ./default.nix);
+  all-hie = (import (fetchTarball "https://github.com/infinisil/all-hies/tarball/master") {});
 in
 haskellPackages.shellFor {
   name = lrwh.env.name;
@@ -13,6 +14,7 @@ haskellPackages.shellFor {
     haskellPackages.hasktags
     haskellPackages.hoogle
     haskellPackages.hindent
+    (all-hie.selection { selector = p: { inherit (p) ghc865; }; })
   ];
 }
 
