a213fd2fec11449802fa3d8bf93ce6425f4a18b2
Xu Shane <xusheng0711@gmail.com>
2019-11-18 00:56:30 +0800

    ch13

---
+++ src/Ch13/DataSequence.hs
@@ -0,0 +1,6 @@
+import qualified Data.Sequence as Seq
+import Data.Sequence ((><), (<|), (|>))
+import qualified Data.Foldable as Foldable
+
+main = undefined
+
---
+++ src/Ch13/buildmap.hs
@@ -0,0 +1,14 @@
+import qualified Data.Map as Map
+
+main = undefined
+
+al = [(1, "one"), (2, "two"), (3, "three"), (4, "four")]
+
+mapFromAL = Map.fromList al
+
+mapFold = foldl (\map (k, v) -> Map.insert k v map) Map.empty al
+
+mapManual =
+  Map.insert 2 "two" .
+  Map.insert 4 "four" . Map.insert 1 "one" . Map.insert 3 "three" $
+  Map.empty
