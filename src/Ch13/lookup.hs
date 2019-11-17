979619ab456d24a60001a71ff20259e646a4918c
Xu Shane <xusheng0711@gmail.com>
2019-11-17 18:14:02 +0800

    ch13

---
+++ src/Ch13/lookup.hs
@@ -0,0 +1,8 @@
+main = undefined
+
+myLookup :: Eq a => a -> [(a, b)] -> Maybe b
+myLookup _ [] = Nothing
+myLookup key ((thiskey, thisval):rest) =
+  if key == thiskey
+    then Just thisval
+    else myLookup key rest
