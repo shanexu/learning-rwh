ff700fdf35b207999ce630ca6df21cb1a9bfa085
Xu Shane <xusheng0711@gmail.com>
2019-11-22 18:24:11 +0800

    change format

--- default.nix
+++ default.nix
@@ -1,4 +1,13 @@
 let
-  pkgs = import <nixpkgs> { };
+  config = {
+    packageOverrides = pkgs: rec {
+      haskellPackages = pkgs.haskellPackages.override {
+        overrides = haskellPackagesNew: haskellPackagesOld: rec {
+          learning-rwh = haskellPackages.callPackage ./learning-rwh.nix { };
+        };
+      };
+    };
+  };
+  pkgs = import <nixpkgs> { inherit config; };
 in
-  pkgs.haskellPackages.callPackage ./learning-rwh.nix { }
+  pkgs.haskellPackages.learning-rwh
