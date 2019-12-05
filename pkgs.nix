4964692f1784889b34faba4b1c11e67974d71871
Xu Shane <xusheng0711@gmail.com>
2019-12-05 09:14:49 +0800

    pin commit

--- pkgs.nix
+++ pkgs.nix
@@ -1,6 +1,6 @@
 let
   all-hies = import (fetchTarball "https://github.com/infinisil/all-hies/tarball/8d9b0e770c8e2264ef4882623a205548d9fdaa03");
-  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs-channels/tarball/nixpkgs-unstable/f97746ba2726128dcf40134837ffd13b4042e95d");
+  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs-channels/tarball/14c1a446d88f6a5369098af2f7e884969085f14d");
 
 in
 { inherit all-hies pkgs;
